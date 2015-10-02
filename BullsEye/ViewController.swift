89e20e98df98da6327646dd4edfce54b748acc5f
Nastaran Fekri <nastaran.fekri@adminisatorspro.training.futuretrainings.com>
2015-10-02 10:39:44 +0200

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Nastaran Fekri on 02.10.15.
+//  Copyright © 2015 Nastaran Fekri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
